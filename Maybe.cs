7306b545c6fa84ea1cf20c1840d0bd0b9fc53748
Tolga Cubukcuoglu <tolga@atolyedijital.com.tr>
2024-04-08 21:09:44 +0300

    Initial commit for github.

---
+++ Maybe.cs
@@ -0,0 +1,233 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+namespace AtolyeDijital;
+
+public class Maybe<T>
+{
+    private readonly T value;
+
+    private readonly bool hasValue;
+
+    private Maybe()
+    {
+        hasValue = false;
+    }
+
+    private Maybe(T value) 
+    {
+        this.value = value;
+        hasValue = true;
+    }
+
+    #region Private Methods
+
+    
+    private Maybe<TResult> GetValueOrThrow<TResult>(Maybe<TResult> val, string errorMessage)
+    {
+        if (val == null || !val.hasValue)
+        {
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                throw new ApplicationException(errorMessage);
+            }
+            return new Maybe<TResult>();
+        }
+        return val;
+    }
+
+    #endregion
+
+    public bool HasValue { get { return hasValue; } }
+
+
+    public Maybe<T> CheckNull(string errorMessage = "")
+    {
+        if (!hasValue)
+        {
+            throw new ApplicationException(errorMessage);
+        }
+        return this;
+    }
+
+    public Maybe<T> Check(Func<T, bool> predicate)
+    {
+        if (!hasValue || !predicate(this.value))
+        {
+            return new Maybe<T>();
+        }
+        return this;
+    }
+
+    public Maybe<T> Check(Func<T, bool> predicate, string errorMessage)
+    {
+        if (!hasValue || !predicate(this.value))
+        {
+            throw new ApplicationException(errorMessage);
+        }
+        return this;
+    }
+
+    public async Task<Maybe<T>> CheckAsync(Func<T, Task<bool>> predicate, string errorMessage)
+    {
+        if (!hasValue)
+        {
+            return this;
+        }
+
+        if (await predicate(this.value))
+        {
+            return this;
+        }
+
+        throw new ApplicationException(errorMessage);
+    }
+
+
+    public static implicit operator Maybe<T>(T value)
+    {
+        if (value == null)
+            return new Maybe<T>();
+
+        return new Maybe<T>(value);
+    }
+
+    public static Maybe<T> from(T value)
+    {
+        if (value == null)
+            return new Maybe<T>();
+
+        return new Maybe<T>(value);
+    }
+
+    public Maybe<TResult> Bind<TResult>(Func<T, Maybe<TResult>> func)
+    {
+        if (!hasValue)
+            return new Maybe<TResult>();
+
+
+        var val = func(value);
+        if (val == null || !val.hasValue)
+        {
+            return new Maybe<TResult>();
+        }
+        return val;
+    }
+
+
+
+    public Maybe<TResult> Bind<TResult>(Func<T, Maybe<TResult>> func, string errorMessage)
+    {
+        if (!hasValue)
+            return new Maybe<TResult>();
+
+        var val = func(value);
+        return GetValueOrThrow(val, errorMessage);
+    }
+
+    public Maybe<TResult> Bind<TResult>(Func<T, Maybe<TResult>> func, string errorMessage, out TResult outVar)
+    {
+        outVar = default;
+        if (!hasValue)
+            return new Maybe<TResult>();
+
+        var val = func(value);
+        val = GetValueOrThrow(val, errorMessage);
+        outVar = val.value;
+        return val;
+    }
+
+
+    public Maybe<TResult> Bind<TResult>(Func<T, TResult> func, string errorMessage = "")
+    {
+        if (!hasValue)
+            return new Maybe<TResult>();
+
+        var val = func(value);
+        if (val == null)
+        {
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                throw new ApplicationException(errorMessage);
+            }
+            else
+            {
+                return new Maybe<TResult>();
+            }
+
+        }
+        return val;
+    }
+
+    [return: NotNull]
+    public T ValueOrThrow(string errorMessage = "Invalid entitiy!")
+    {
+        if (hasValue)
+            return value!;
+
+        throw new ApplicationException(errorMessage);
+    }
+
+    #region Async
+    public async Task<Maybe<TResult>> BindAsync<TResult>(Func<T, Task<Maybe<TResult>>> func)
+    {
+        if (!hasValue)
+            return await Task.FromResult(new Maybe<TResult>());
+
+
+        var val = await func(value);
+        if (val == null || !val.hasValue)
+        {
+            return new Maybe<TResult>();
+        }
+        return val;
+    }
+
+    public async Task<Maybe<TResult>> BindAsync<TResult>(Func<T, Task<Maybe<TResult>>> func, string errorMessage)
+    {
+        if (!hasValue)
+            return await Task.FromResult(new Maybe<TResult>());
+
+        var val = await func(value);
+        return GetValueOrThrow(val, errorMessage);
+    }
+
+    public async Task<Maybe<TResult>> BindAsync<TResult>(Func<T, Maybe<TResult>> func, string errorMessage)
+    {
+        if (!hasValue)
+            return await Task.FromResult(new Maybe<TResult>());
+
+        var val = await Task.FromResult(func(value));
+        return GetValueOrThrow(val, errorMessage);
+    }
+
+
+    public async Task<Maybe<TResult>> BindAsync<TResult>(Func<T, Task<TResult>> func)
+    {
+        return await BindAsync(func, string.Empty);
+    }
+
+    public async Task<Maybe<TResult>> BindAsync<TResult>(Func<T, Task<TResult>> func, string errorMessage)
+    {
+        if (!hasValue)
+            return await Task.FromResult(new Maybe<TResult>());
+
+        var val = await func(value);
+        if (val == null)
+        {
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                throw new ApplicationException(errorMessage);
+            }
+            else
+            {
+                return new Maybe<TResult>();
+            }
+
+        }
+        return val;
+    }
+    #endregion
+}
+
+
---
+++ TaskExtensions.cs
@@ -0,0 +1,55 @@
+﻿using AtolyeDijital;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtolyeDijital;
+public static class TaskExtensions
+{
+    public static async Task<Maybe<TResult>> Bind<T, TResult>(
+        this Task<Maybe<T>> task, Func<T, Maybe<TResult>> func)
+        => (await task).Bind(func);
+
+    public static async Task<Maybe<TResult>> Bind<T, TResult>(
+        this Task<Maybe<T>> task, Func<T, Maybe<TResult>> func, string errorMessage)
+        => (await task).Bind(func, errorMessage);
+
+    public static async Task<Maybe<TResult>> BindAsync<T, TResult>(
+        this Task<Maybe<T>> task, Func<T, Task<Maybe<TResult>>> func)
+        => await (await task).BindAsync(func);
+
+    public static async Task<Maybe<TResult>> BindAsync<T, TResult>(
+        this Task<Maybe<T>> task, Func<T, Task<Maybe<TResult>>> func, string errorMessage)
+        => await (await task).BindAsync(func, errorMessage);
+
+    public static async Task<Maybe<TResult>> BindAsync<T, TResult>(
+        this Task<Maybe<T>> task, Func<T, Task<TResult>> func)
+        => await (await task).BindAsync(func);
+
+    public static async Task<Maybe<TResult>> BindAsync<T, TResult>(
+        this Task<Maybe<T>> task, Func<T, Task<TResult>> func, string errorMessage)
+        => await (await task).BindAsync(func, errorMessage);
+
+    [return: NotNull]
+    public async static Task<T> ValueOrThrow<T>(
+        this Task<Maybe<T>> task, string errorMessage)
+        => (await task).ValueOrThrow(errorMessage);
+
+    public async static Task<Maybe<T>> Check<T>(
+        this Task<Maybe<T>> task, Func<T, bool> predicate)
+        => (await task).Check(predicate);
+
+    public async static Task<Maybe<T>> Check<T>(
+        this Task<Maybe<T>> task, Func<T, bool> predicate, string errorMessage)
+        => (await task).Check(predicate, errorMessage);
+    public async static Task<Maybe<T>> CheckAsync<T>(
+    this Task<Maybe<T>> task, Func<T, Task<bool>> predicate, string errorMessage)
+    {
+        var maybe = await task;
+        return await maybe.CheckAsync(predicate, errorMessage);
+    }
+
+}
